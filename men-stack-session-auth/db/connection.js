2fe4f339f130046299ed86daeb1463ffe909d77a
camiegray <camieg4@gmail.com>
2025-03-28 07:09:23 -0700

    Initial commit

---
+++ men-stack-session-auth/db/connection.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import chalk from "chalk";
+
+mongoose.set("returnOriginal", false);
+
+mongoose.connect(process.env.MONGODB_URI).catch((err) => {
+  console.log(`Error connection to MongoDB: ${err.message}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log(chalk.bold("Disconnected from MongoDB!"));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(chalk.red(`MongoDB connection error: ${err.message}`));
+});
+
+export default mongoose.connection;
